bfca9d0ab6c42488d78b936615edc3f452ba3738
Avalon472 <mjakob478@gmail.com>
2025-09-11 21:50:54 -0500

    Added game page and placeholder elements

--- effects.js
+++ effects.js
@@ -1,3 +1,4 @@
+//Scripting for cursor effect
 const cursor = document.querySelector(".cursor");
 
 document.addEventListener("mousemove", (e) => {
@@ -8,11 +9,27 @@ document.addEventListener("mousemove", (e) => {
     cursor.style.left = x + "px";})
     
 
+//Scripting for start game button
+const startButton = document.getElementById('start_Button')
+    if(startButton){
+    startButton.addEventListener('click', function(e) {
+        e.preventDefault();
+        const button = this;
+        const anchor = button.closest('a')
+        button.classList.add('pulsing');
 
-document.getElementById('start_Button').addEventListener('click', function() {
-const button = this;
-button.classList.add('pulsing');
+        button.addEventListener('animationend', function() {
+        button.classList.remove('pulsing');
+        window.location.href = anchor.href;}, { once: true });
+    });
+}
 
-button.addEventListener('animationend', function() {
-    button.classList.remove('pulsing');}, { once: true });
+// Scripting for choice buttons
+document.querySelectorAll('.choice').forEach(choice => {
+  choice.addEventListener('click', function (e) {
+    e.preventDefault();
+    const value = this.dataset.value;
+    console.log("You chose:", value);
+    // now do something with "value"
+  });
 });
