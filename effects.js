a6d8a8656f456e77695ddec48792f12823edb60c
Avalon472 <mjakob478@gmail.com>
2025-09-15 17:28:32 -0500

    Added game logic, will update visual effects

--- effects.js
+++ effects.js
@@ -24,15 +24,94 @@ const startButton = document.getElementById('start_Button')
     });
 }
 
+//*************************************************************************************************************************************
+// Code for Game Logic
+//*************************************************************************************************************************************
+
+
+//Global varible for player choice and scores
+let pChoice = null;
+let pWins = 0;
+let cWins = 0;
+
 // Scripting for choice buttons
 document.querySelectorAll('.choice').forEach(choice => {
   choice.addEventListener('click', function (e) {
     e.preventDefault();
     const value = this.dataset.value;
     console.log("You chose:", value);
-    setImage(value, "PSelect");
+    if(pWins != 5 && cWins != 5){
+      playRound(value);
+      setImage(value, "PSelect");}
   });
 });
+
+document.addEventListener("DOMContentLoaded", () => {
+  let status = document.getElementById("status");
+  status.innerHTML = "<h1> Let the game begin! <h1>"
+})
+
+function compSelect(){
+    let choice = Math.random();
+    if(choice <= 0.33 && choice >= 0){
+      setImage("sword", "CSelect")
+      return "sword"
+    }
+    else if(choice >0.33 && choice < 0.67){
+      setImage("spear", "CSelect")
+      return "spear"
+    }
+    else if(choice >=0.67 && choice <= 1){
+      setImage("axe", "CSelect")
+      return "axe"
+    }
+}
+
+function playRound(humanChoice) {
+  let PScore = document.getElementById("PScore");
+  let CScore = document.getElementById("CScore");
+  let status = document.getElementById("status")
+  PScore.innerHTML = "";
+  CScore.innerHTML = "";
+  status.innerHTML = "";
+  let message = "";
+  compChoice = compSelect();
+    if (humanChoice === compChoice)
+        message = "The round ends in a tie"
+    else if(humanChoice == "sword"){
+        if(compChoice == "spear"){
+          message = "Sword beats Spear. You win the round!"
+          pWins++;
+        }
+        else{
+          message = "Axe beats Sword. You lose the round."
+          cWins++
+        }
+    }
+    else if(humanChoice == "spear"){
+        if(compChoice == "axe"){
+          message = "Spear beats Axe. You win the round!"
+          pWins++}
+        else{
+            message = "Sword beats Spear. You lose the round."
+          cWins++;}
+    }
+    else if(humanChoice == "axe"){
+        if(compChoice == "sword"){
+            message = "Axe beats Sword. You win the round!"
+          pWins++}
+        else{
+            message = "Spear beats Axe. You lose the round."
+          cWins++;}
+    }
+    if(pWins == 5)
+      message = "You have won the game! Press restart to play again."
+    else if (cWins == 5)
+      message = "You have lost the game. Press restart to try again."
+    status.innerHTML = "<h1>" + message + "<h1>";
+    PScore.innerHTML = "<h1>" + pWins + "<h1>";
+    CScore.innerHTML = "<h1>" + cWins + "<h1>";
+}
 
 function setImage(input, parent){
   const container = document.getElementById(parent);
